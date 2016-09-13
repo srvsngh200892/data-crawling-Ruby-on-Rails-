d5c56aebcc5aa061dabf2d65da3bdaa0f93696d9
srvsngh200892 <srvsngh200892@gmail.com>
2016-09-13 21:15:30 +0530

    bug:fixed empty json condition

--- app/controllers/university/universities_controller.rb
+++ app/controllers/university/universities_controller.rb
@@ -1,7 +1,8 @@
 class University::UniversitiesController < ApplicationController
 
   def index
-    @university_list = $university
-    @res = Kaminari.paginate_array(@university_list, total_count: 311).page(params[:page]).per(10)
+    university_list = $university.present? ? $university : []
+
+    @res = Kaminari.paginate_array(university_list, total_count: 311).page(params[:page]).per(10)
   end
 end
